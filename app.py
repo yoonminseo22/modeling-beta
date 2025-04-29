78affb6fe88c4b944b945616e39f155475256ee5
yoonminseo22 <zdnlsz1@gmail.com>
2025-04-29 09:19:25 +0900

    1

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ import hashlib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.font_manager as fm
-import os
+import matplotlib.dates as mdates
 from datetime import datetime
 from oauth2client.service_account import ServiceAccountCredentials
 
